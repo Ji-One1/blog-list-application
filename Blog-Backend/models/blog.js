2dd4871c98bb19ba907e063675a982a9efa60bbe
Jione <jioneban@gmail.com>
2023-09-21 14:20:36 -0400

    set up blog.js

---
+++ Blog-Backend/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+    .then(result => console.log('connected to mongo'))
+    .catch(err => console.log('error:', err.message))
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
