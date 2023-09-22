120bc6401045f6d9d5101cb0d00f83c3c7d98539
Jione <jioneban@gmail.com>
2023-09-21 20:58:32 -0400

    create helper functions

--- Blog-Backend/utils/list_helper.js
+++ Blog-Backend/utils/list_helper.js
@@ -1,4 +1,25 @@
 const dummy = (blogs) => {
 	return 1
 }
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+	const reducer = (sum, item) => {
+		return sum + item.likes
+	}
+	return blogs.length === 0
+		? 0
+		: blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+	return blogs.reduce((favorite, blog) => {
+		return blog.likes > favorite.likes ? blog : favorite}, blogs[0])
+}
+
+
+
+
+
+
+
+
+module.exports = { dummy, totalLikes, favoriteBlog }
